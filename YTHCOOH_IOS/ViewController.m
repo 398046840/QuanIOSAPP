02eb8049a735e4483f64c2da321c84ca0b682121
398046840 <398046840@qq.com>
2015-01-08 11:29:20 +0800

    Initial Commit

---
+++ YTHCOOH_IOS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YTHCOOH_IOS
+//
+//  Created by 宝发 on 15-1-8.
+//  Copyright (c) 2015年 AFABLE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
